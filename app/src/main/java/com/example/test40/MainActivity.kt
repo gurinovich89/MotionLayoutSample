5a123563a0e3a48cba27ba5e3dd1acb974049a4b
a.gurinovich89 <a.gurinovich89@gmail.com>
2019-12-04 00:12:38 +0300

    login animation

--- app/src/main/java/com/example/test40/MainActivity.kt
+++ app/src/main/java/com/example/test40/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.motion.widget.MotionLayout
+import androidx.core.view.setPadding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -12,20 +13,11 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         btn_start.setOnClickListener({ startFirstAnimation() })
+        tv_emin.setPadding(1)
     }
 
 
     private fun startFirstAnimation() {
-        if (true) {
-            iv_logo.performClick()
-            return
-        }
-        if (true) {
-            val intent = Intent(applicationContext, MotionActivity::class.java)
-            startActivity(intent)
-            return
-        }
-        motion_layout_root.transitionToEnd()
         motion_layout_root.setTransitionListener(object : MotionLayout.TransitionListener {
             override fun onTransitionTrigger(p0: MotionLayout?, p1: Int, p2: Boolean, p3: Float) {
 
@@ -39,9 +31,36 @@ class MainActivity : AppCompatActivity() {
 
             }
 
-            override fun onTransitionCompleted(p0: MotionLayout?, p1: Int) {
-
+            override fun onTransitionCompleted(motionLayout: MotionLayout?, currentId: Int) {
+                when (currentId) {
+                    R.id.state_end1 -> {
+                        motionLayout?.apply {
+                            this.setTransition(R.id.state_end1, R.id.state_end2)
+                            this.setTransitionDuration(800)
+                            this.transitionToEnd()
+                        }
+                    }
+                    R.id.state_end2 -> {
+                        motionLayout?.apply {
+                            /*this.setTransition(R.id.state_end2, R.id.state_end3)
+                            this.setTransitionDuration(3000)
+                            this.transitionToEnd()*/
+                            view_transition3.performClick()
+                        }
+                    }
+                }
             }
+
         })
+        if (true) {
+            view_transition1.performClick()
+            return
+        }
+        if (true) {
+            val intent = Intent(applicationContext, MotionActivity::class.java)
+            startActivity(intent)
+            return
+        }
+        motion_layout_root.transitionToEnd()
     }
 }
