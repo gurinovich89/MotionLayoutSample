22d7f2c026f386af077f27b025af0ddfad48c832
a.gurinovich89 <a.gurinovich89@gmail.com>
2019-12-07 14:12:29 +0300

    pincode animation

--- app/src/main/java/com/example/test40/MainFragment.kt
+++ app/src/main/java/com/example/test40/MainFragment.kt
@@ -6,7 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.constraintlayout.motion.widget.MotionLayout
 import androidx.fragment.app.Fragment
-import kotlinx.android.synthetic.main.fragment_main.*
+import kotlinx.android.synthetic.main.fragment_main_pincode.*
 
 
 class MainFragment : Fragment() {
@@ -19,7 +19,7 @@ class MainFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_main_pincode, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
